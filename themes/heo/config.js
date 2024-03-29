f9400cd9fc8511f182fff828f26d1e52f974f835
yaney <53202722+yaney01@users.noreply.github.com>
2024-03-29 14:19:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,12 +19,12 @@ const CONFIG = {
   HEO_HERO_TITLE_3: '欢迎加入我的电报群',
   HEO_HERO_TITLE_4: '每日更新节点',
   HEO_HERO_TITLE_5: 'Yaney Little Courtyard',
-  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01',,
+  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选'， url: '/tag/必看精选' }，
+  HEO_HERO_CATEGORY_2: { title: '热门文章'， url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程'， url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
