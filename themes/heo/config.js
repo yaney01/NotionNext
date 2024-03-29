6c7643fe21a66860c22c88431e51269b1562a87c
yaney <53202722+yaney01@users.noreply.github.com>
2024-03-29 14:12:49 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '欢迎来到我的博客', url: 'https://blog.yaney.eu.org/' },
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -19,12 +19,12 @@ const CONFIG = {
   HEO_HERO_TITLE_3: '欢迎加入我的电报群',
   HEO_HERO_TITLE_4: '每日更新节点',
   HEO_HERO_TITLE_5: 'Yaney Little Courtyard',
-  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01,
+  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选'， url: '/tag/必看精选' }，
+  HEO_HERO_CATEGORY_2: { title: '热门文章'， url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程'， url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
