12014698435f0df55e5cf28910e6fed69c7a5245
yaney <53202722+yaney01@users.noreply.github.com>
2024-08-31 12:04:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://yaney.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,12 +17,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享设计',
+  HEO_HERO_TITLE_2: 'AI资料库',
+  HEO_HERO_TITLE_3: '欢迎加入我的电报群',
+  HEO_HERO_TITLE_4: '每日更新节点',
+  HEO_HERO_TITLE_5: 'Yaney Little Courtyard',
+  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
