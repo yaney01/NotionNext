6046211644c5700d3b914f0467c1c97eeeabca76
yaney <53202722+yaney01@users.noreply.github.com>
2025-02-06 10:52:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://yaney.top' },
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,12 +17,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享设计',
+  HEO_HERO_TITLE_2: 'AI资料库',
+  HEO_HERO_TITLE_3: '欢迎加入我的电报群',
+  HEO_HERO_TITLE_4: '每日更新节点',
+  HEO_HERO_TITLE_5: 'Yaney Little Courtyard',
+  HEO_HERO_TITLE_LINK: 'https://t.me/yaney_01',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -51,9 +50,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: 'https://x.com/yaney_designy',
+  HEO_INFO_CARD_ICON2: 'fab fa-twitter',
+  HEO_INFO_CARD_URL3: 'https://www.yaney.top',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
